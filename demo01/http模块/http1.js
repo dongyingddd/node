f1d3667a8898e54c51470565fc86ac3f24960b86
dongyingddd <2273010094@qq.comgit config --global user.email 2273010094@qq.com>
2020-02-11 15:52:54 +0800

    核心模块http模块的基本使用

---
+++ demo01/http模块/http1.js
@@ -0,0 +1,18 @@
+//引入核心模块http
+const http = require('http');
+
+//创建服务
+//回调函数:在每一次收到来自客户端的请求,它都会执行一次
+const server = http.createServer(function (req, res) {
+    //获取访问本服务器的客户端ip
+    console.log(req.connection.remoteAddress);
+
+    //向客户端发送内容,并结束本次响应
+    res.end('hello world');
+});
+
+//启动服务
+//监听端口
+server.listen(8081, function () {
+    console.log("别用鼠标选中..........本次服务已经启动了,success");
+});
