af1a37455e2975474c9310ba7c5c5b48be3c6ddb
dongyingddd <2273010094@qq.comgit config --global user.email 2273010094@qq.com>
2020-02-13 20:36:36 +0800

    从json文件中读出数据返回

--- code/接口服务器/app.js
+++ code/接口服务器/app.js
@@ -2,15 +2,19 @@
 //引入核心模块
 const http = require('http');
 
+//引入自定义模块
+const msg = require('./msg');
+
 //创建服务器
 const server = http.createServer((req, res) => {
 
     if (req.url === '/getmsg' && req.method === 'GET') {
         //假设经过很多运算得到数据
-        let data = [
-            { id: 1, name: '张三', conent: '寒雨连江夜入吴', dt: 12343544354 },
-            { id: 2, name: '李四', conent: '窗前明月光', dt: 123435444567 },
-        ];
+        // let data = [
+        //     { id: 1, name: '张三', conent: '寒雨连江夜入吴', dt: 12343544354 },
+        //     { id: 2, name: '李四', conent: '窗前明月光', dt: 123435444567 },
+        // ];
+        let data = msg.get();
         res.setHeader('content-type', 'application/json');
         res.end(JSON.stringify(data));
     } else {
