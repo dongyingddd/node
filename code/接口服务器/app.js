cedf6c88a00540ff767ac019e2e1d31740d1320e
dongyingddd <2273010094@qq.comgit config --global user.email 2273010094@qq.com>
2020-02-13 20:32:12 +0800

    不带参数的get类型的接口服务器

---
+++ code/接口服务器/app.js
@@ -0,0 +1,24 @@
+
+//引入核心模块
+const http = require('http');
+
+//创建服务器
+const server = http.createServer((req, res) => {
+
+    if (req.url === '/getmsg' && req.method === 'GET') {
+        //假设经过很多运算得到数据
+        let data = [
+            { id: 1, name: '张三', conent: '寒雨连江夜入吴', dt: 12343544354 },
+            { id: 2, name: '李四', conent: '窗前明月光', dt: 123435444567 },
+        ];
+        res.setHeader('content-type', 'application/json');
+        res.end(JSON.stringify(data));
+    } else {
+        res.end('404');
+    }
+});
+
+//启动端口监听
+server.listen(8084, () => {
+    console.log("亲爱的,您的服务器在8084端口启动.....");
+});
