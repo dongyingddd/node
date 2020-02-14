983fb843b5b7858b0a0888843b7bb57edfb8f3ea
dongyingddd <2273010094@qq.comgit config --global user.email 2273010094@qq.com>
2020-02-14 15:33:26 +0800

    fs模块-文件内容读取的同步和异步格式

---
+++ code/03.使用fs模块.js
@@ -0,0 +1,24 @@
+//使用核心模块的步骤
+//1. 引入模块
+//require('fs)是把fs这个模块中的代码执行一次,并把执行结果保存在常量fs中
+const fs = require('fs');
+
+
+
+//2.使用模块的api
+
+//2.1 文件内容读取
+
+//假设目标是要读出当前文件夹在的01.js的内容
+//异步格式
+fs.readFile('01.js', 'utf-8', (err, data) => {
+    if (err) {
+        throw err
+    }
+    console.log(data);
+
+});
+
+//同步格式
+// const jsHtml = fs.readFileSync('01.js', 'utf8');
+// console.log(jsHtml);
